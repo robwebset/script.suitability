9fcf8bfa835070a63cf8aa8c745d93017865ff9d
robwebset <robwebset@users.noreply.github.com>
2016-12-11 08:08:22 +0000

    Update for support of URepo

--- default.py
+++ default.py
@@ -44,37 +44,46 @@ if __name__ == '__main__':
         if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.robwebset') and (addonItem['author'] == 'robwebset'):
             displayNotice = False
 
-            videoName = None
-            isTvShow = getIsTvShow()
+    if displayNotice:
+        json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Addons.GetAddonDetails", "params": { "addonid": "repository.urepo", "properties": ["enabled", "broken", "name", "author"]  }, "id": 1}')
+        json_response = json.loads(json_query)
 
-            # First check to see if we have a TV Show of a Movie
-            if isTvShow:
-                videoName = xbmc.getInfoLabel("ListItem.TVShowTitle")
-
-            # If we do not have the title yet, get the default title
-            if videoName in [None, ""]:
-                videoName = xbmc.getInfoLabel("ListItem.Title")
-
-            # If there is no video name available prompt for it
-            if videoName in [None, ""]:
-                # Prompt the user for the new name
-                keyboard = xbmc.Keyboard('', ADDON.getLocalizedString(32032), False)
-                keyboard.doModal()
-
-                if keyboard.isConfirmed():
-                    try:
-                        videoName = keyboard.getText().decode("utf-8")
-                    except:
-                        videoName = keyboard.getText()
-
-            if videoName not in [None, ""]:
-                log("Suitability: Video detected %s" % videoName)
-                SuitabilityCore.runForVideo(videoName, isTvShow)
-            else:
-                log("Suitability: Failed to detect selected video")
-                xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32011))
+        if ("result" in json_response) and ('addon' in json_response['result']):
+            addonItem = json_response['result']['addon']
+            if (addonItem['enabled'] is True) and (addonItem['broken'] is False) and (addonItem['type'] == 'xbmc.addon.repository') and (addonItem['addonid'] == 'repository.urepo'):
+                displayNotice = False
 
     if displayNotice:
-        xbmc.executebuiltin('Notification("robwebset Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
+        xbmc.executebuiltin('Notification("robwebset of URepo Repository Required","github.com/robwebset/repository.robwebset",10000,%s)' % ADDON.getAddonInfo('icon'))
+    else:
+        videoName = None
+        isTvShow = getIsTvShow()
+
+        # First check to see if we have a TV Show of a Movie
+        if isTvShow:
+            videoName = xbmc.getInfoLabel("ListItem.TVShowTitle")
+
+        # If we do not have the title yet, get the default title
+        if videoName in [None, ""]:
+            videoName = xbmc.getInfoLabel("ListItem.Title")
+
+        # If there is no video name available prompt for it
+        if videoName in [None, ""]:
+            # Prompt the user for the new name
+            keyboard = xbmc.Keyboard('', ADDON.getLocalizedString(32032), False)
+            keyboard.doModal()
+
+            if keyboard.isConfirmed():
+                try:
+                    videoName = keyboard.getText().decode("utf-8")
+                except:
+                    videoName = keyboard.getText()
+
+        if videoName not in [None, ""]:
+            log("Suitability: Video detected %s" % videoName)
+            SuitabilityCore.runForVideo(videoName, isTvShow)
+        else:
+            log("Suitability: Failed to detect selected video")
+            xbmcgui.Dialog().ok(ADDON.getLocalizedString(32001), ADDON.getLocalizedString(32011))
 
     log("Suitability: Ended")
